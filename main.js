22edb07a66864d2196fcbf9e758f3a071e7cc32c
Peter Flynn <pflynn@adobe.com>
2018-11-29 12:26:36 -0800

    Arrow key navigation to highlight & provisionally go to an artboard while dialog is still open. (Canceling dialog with Esc reverts to original view).
    If filtering results in only 1 artboard in list, we select & go to that
    artboard immediately without any arrow keys. If multiple hits, we don't move
    the view immediately but pressing Enter will still go to the top hit - so
    we semi-highlight the top hit to indicate this.
    
    Populate list view immediately on open, which allows for pure arrow-key
    nav with no typing in simple documents.

--- main.js
+++ main.js
@@ -24,8 +24,19 @@ const Viewport = require("viewport");
 
 const DIALOG_CANCELED = "reasonCanceled";
 
+var origViewport;
 var candidates;
 var filtered;
+var selectedI;
+
+// General behavior:
+// - Type to filter artboards by name; top 5 hits are shown (dialogs can't resize yet)
+// - Top item semi-highlighted to indicate pressing Enter will go to it
+// - Arrow keys to select: pans to artboard immediately (then nothing more happens on Enter)
+//    - Editing filter text after using arrows to select tries to keep the same item selected if it's still in the top results
+// - If only one result after filtering, select & go to it automatically
+// - Closing dialog via Escape reverts viewport to original position
+
 
 function escape(text) {
     return text.replace(/&/g, "&amp;").replace(/</g, "&lt;");
@@ -35,19 +46,37 @@ function gatherCandidates(root) {
     candidates = root.children.filter(node => node instanceof Artboard);
 }
 
-function updateList(filterText) {
-    filterText = filterText.toLowerCase();
-    filtered = candidates.filter(node => node.name.toLowerCase().indexOf(filterText) !== -1);
-    filtered = filtered.slice(0, 5);
-
+function rerenderList() {
     var listItemsHTML = "";
-    filtered.forEach(node => {
-        listItemsHTML +=  `<div>${escape(node.name)}</div>`;
+    filtered.forEach((node, i) => {
+        var cssClass = (i === selectedI)? "selected" : (i === 0 && selectedI === -1)? "onDeck" : "";
+        listItemsHTML +=  `<div class="${cssClass}">${escape(node.name)}</div>`;
     });
     document.getElementById("list").innerHTML = listItemsHTML;
 }
 
+function updateList(filterText) {
+    var selectedArtboard = filtered[selectedI];  // undefined if nothing selected
+
+    filterText = filterText.toLowerCase();
+    filtered = candidates.filter(node => node.name.toLowerCase().indexOf(filterText) !== -1);
+    filtered = filtered.slice(0, 5);
+
+    if (filtered.length === 1) {
+        selectedI = 0;
+        gotoArtboard(filtered[0]);
+    } else {
+        // If last selected artboard is still in filtered results, keep it selected if possible
+        selectedI = filtered.indexOf(selectedArtboard);
+        // (will be -1 if nothing was selected before, or if selected artboard no longer in filtered results)
+    }
+    rerenderList();
+}
+
 function quickNav(selection, root) {
+    filtered = [];
+    selectedI = -1;
+
     var dialog = document.createElement("dialog");
     dialog.innerHTML = `
         <style>
@@ -61,8 +90,17 @@ function quickNav(selection, root) {
         }
         #list div {
             height: 35px;
-            padding: 5px;
+            padding: 9px 5px 2px 5px;
             font-size: 12px;
+        }
+        #list div.selected {
+            background: #d8d8d8;
+            border-radius: 5px;
+        }
+        #list div.onDeck {
+            border: 1px solid #a0a0a0;
+            padding: 8px 4px 1px 4px; /* compensate for border */
+            border-radius: 5px;
         }
         </style>
         <form method="dialog">
@@ -71,22 +109,32 @@ function quickNav(selection, root) {
             <div class="row">
                 <input type="text" uxp-quiet="true" id="input" />
             </div>
-            <div id="list">
-                <!-- since dialogs can't resize yet, allocate fixed space for 5 top 'hits' -->
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
+            <div id="list"></div>
         </form>`;
     document.appendChild(dialog);
 
+    origViewport = Viewport.bounds;
     gatherCandidates(root);
+    updateList("");
 
     var input = document.getElementById("input");
     input.addEventListener("input", function () {
         updateList(input.value);
+    });
+    input.addEventListener("keydown", function (event) {
+        if (event.code === "ArrowDown") {
+            if (filtered.length && selectedI < filtered.length - 1) {
+                selectedI++;
+                gotoArtboard(filtered[selectedI]);
+                rerenderList();
+            }
+        } else if (event.code === "ArrowUp") {
+            if (filtered.length && selectedI > 0) {
+                selectedI--;
+                gotoArtboard(filtered[selectedI]);
+                rerenderList();
+            }
+        }
     });
 
     // Enter key automatically 'submits' the form
@@ -94,7 +142,9 @@ function quickNav(selection, root) {
     return dialog.showModal().then(function (reason) {
         dialog.remove();
 
-        if (reason !== DIALOG_CANCELED && filtered.length > 0) {
+        if (reason === DIALOG_CANCELED) {
+            Viewport.scrollToCenter(origViewport.x + origViewport.width / 2, origViewport.y + origViewport.height / 2);
+        } else if (filtered.length > 0 && selectedI === -1) {
             gotoArtboard(filtered[0]);
         }
     });
